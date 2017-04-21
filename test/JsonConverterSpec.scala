9b846e70fc4e208448852792d933ff7cef6795e3
Stan Reshetnyk <sr@trembit.com>
2017-04-21 11:53:04 +0300

    Fix json conversion test

--- test/JsonConverterSpec.scala
+++ test/JsonConverterSpec.scala
@@ -1,10 +1,13 @@
 import actor.utils.{Converter, ChangeName, ChangeBracketMessage}
+import org.junit.runner.RunWith
 import org.specs2.Specification
+import org.specs2.runner.JUnitRunner
 import play.api.libs.json.{JsValue, JsObject, JsString}
 
 /**
   * Created by Stan Reshetnyk on 20.04.17.
   */
+@RunWith(classOf[JUnitRunner])
 class JsonConverterSpec extends Specification {
 
   def is = s2"""
@@ -14,7 +17,7 @@ class JsonConverterSpec extends Specification {
       ChangeName                                       $e2
                                                       """
 
-  val e1 = (ChangeBracketMessage("stan", "", JsString("123")).toJson.toString()) must equalTo("123")
+  val e1 = (ChangeBracketMessage("stan", "", JsString("123")).toJson.toString()) must equalTo("""{"bracket":"stan","key":"stan.","messageType":"change","id":"","value":"123"}""")
 
   private val jsObject: JsValue = JsObject(Seq("messageType" -> JsString("changeName"), "name" -> JsString("stan")))
   val e2 = (Converter.toMessage(jsObject).get) must equalTo(ChangeName("stan"))
